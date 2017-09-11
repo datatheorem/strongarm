c8e10e5cc373107a9bff8e995fb92dee27dc1372
Phillip Tennen <pt@datatheorem.com>
2017-09-11 14:21:37 -0700

    Add docstrings to MachO parser

--- strongarm/macho_binary.py
+++ strongarm/macho_binary.py
@@ -4,6 +4,7 @@ import macho_load_commands
 
 class MachoBinary(object):
     def __init__(self, fat_file, offset_within_fat=0):
+        # type: (file, int) -> MachoBinary
         self._file = fat_file
         self.offset_within_fat = offset_within_fat
 
@@ -24,6 +25,11 @@ class MachoBinary(object):
         self.parse()
 
     def parse(self):
+        """
+        Attempt to parse the provided file contents as a MachO slice
+        This method may throw an exception if the provided data does not represent a valid or supported
+        Mach-O slice.
+        """
         if not self.check_magic():
             return
         self.is_swap = self.should_swap_bytes()
@@ -34,6 +40,13 @@ class MachoBinary(object):
         self.parse_header()
 
     def check_magic(self):
+        """
+        Ensure magic at provided offset within provided file represents a valid and supported Mach-O slice
+        Sets up byte swapping if host and slice differ in endianness
+        This method will throw an exception if the magic is invalid or unsupported
+        Returns:
+            True if the magic represents a supported file format
+        """
         self.magic = c_uint32.from_buffer(bytearray(self.get_bytes(0, sizeof(c_uint32)))).value
         valid_mag = [
             MachArch.MH_MAGIC_64,
@@ -55,9 +68,18 @@ class MachoBinary(object):
         return True
 
     def magic_is_64(self):
+        """
+        Convenience method to check if our magic corresponds to a 64-bit slice
+        Returns:
+            True if self.magic corresponds to a 64 bit MachO slice, False otherwise
+        """
         return self.magic == MachArch.MH_MAGIC_64 or self.magic == MachArch.MH_CIGAM_64
 
     def parse_header(self):
+        """
+        Parse all info from a Mach-O header.
+        This method will also parse all segment and section commands.
+        """
         header_bytes = self.get_bytes(0, sizeof(MachoHeader64))
         self.header = MachoHeader64.from_buffer(bytearray(header_bytes))
 
@@ -69,10 +91,16 @@ class MachoBinary(object):
             self.cpu_type = CPU_TYPE.UNKNOWN
 
         self._num_commands = self.header.ncmds
+        # load commands begin directly after Mach O header
         self.load_commands_offset = sizeof(MachoHeader64)
         self.parse_segment_commands(self.load_commands_offset)
 
     def parse_segment_commands(self, offset):
+        """
+        Parse Mach-O segment commands beginning at a given slice offset
+        Args:
+            offset: Slice offset to first segment command
+        """
         for i in range(self._num_commands):
             load_command_bytes = self.get_bytes(offset, sizeof(MachOLoadCommand))
             load_command = MachOLoadCommand.from_buffer(bytearray(load_command_bytes))
@@ -81,15 +109,20 @@ class MachoBinary(object):
                 # 32 bit segments unsupported!
                 continue
 
+            # some commands have their own structure that we interpret seperately from a normal load command
+            # if we want to interpret more commands in the future, this is the place to do it
             if load_command.cmd == MachoLoadCommands.LC_SYMTAB:
                 symtab_bytes = self.get_bytes(offset, sizeof(MachoSymtabCommand))
                 self.symtab = MachoSymtabCommand.from_buffer(bytearray(symtab_bytes))
+
             elif load_command.cmd == MachoLoadCommands.LC_DYSYMTAB:
                 dysymtab_bytes = self.get_bytes(offset, sizeof(MachoDysymtabCommand))
                 self.dysymtab = MachoDysymtabCommand.from_buffer(bytearray(dysymtab_bytes))
+
             elif load_command.cmd == MachoLoadCommands.LC_ENCRYPTION_INFO_64:
                 encryption_info_bytes = self.get_bytes(offset, sizeof(MachoEncryptionInfo64Command))
                 self.encryption_info = MachoEncryptionInfo64Command.from_buffer(bytearray(encryption_info_bytes))
+
             elif load_command.cmd == MachoLoadCommands.LC_SEGMENT_64:
                 segment_bytes = self.get_bytes(offset, sizeof(MachoSegmentCommand64))
                 segment = MachoSegmentCommand64.from_buffer(bytearray(segment_bytes))
@@ -97,41 +130,91 @@ class MachoBinary(object):
                 self.segments[segment.segname] = segment
                 self.parse_sections(segment, offset)
 
+            # move to next load command in header
             offset += load_command.cmdsize
 
     def parse_sections(self, segment, segment_offset):
+        # type: (MachoSegmentCommand64, int) -> None
+        """
+        Parse all sections contained within a Mach-O segment,
+        and add them to our map of sections
+        Args:
+            segment: The segment command whose sections should be read
+            segment_offset: The offset within the file that the segment command is located at
+        """
         if not segment.nsects:
             return
 
+        # the first section of this segment begins directly after the segment
         section_offset = segment_offset + sizeof(MachoSegmentCommand64)
         section_size = sizeof(MachoSection64)
+
         for i in range(segment.nsects):
             section_bytes = self.get_bytes(section_offset, sizeof(MachoSection64))
             section = MachoSection64.from_buffer(bytearray(section_bytes))
             # TODO(pt) handle byte swap of segment
+            # add to our section with the section name as the key
             self.sections[section.sectname] = section
 
             section_offset += section_size
 
     def get_section_with_name(self, name):
+        # type: (str) -> Optional[MachoSection64]
+        """
+        Convenience method to retrieve a section with a given name from map
+        Args:
+            name: The name of the section to find
+        Returns:
+            The MachoSection64 command if it is found, None otherwise
+        """
         if name in self.sections:
             return self.sections[name]
         return None
 
     def get_section_content(self, section):
+        # type: (MachoSection64) -> bytearray
+        """
+        Convenience method to retrieve slice content associated with a section command
+        Args:
+            section: The section command whose corresponding content should be found
+        Returns:
+            bytearray containing the file contents associated with the section command
+        """
         return bytearray(self.get_bytes(section.offset, section.size))
 
     def get_virtual_base(self):
+        # type: (None) -> (int)
+        """
+        Retrieve the first virtual address of the Mach-O slice
+        Returns:
+            int containing the virtual memory space address that the Mach-O slice requests to begin at
+        """
         text_seg = self.segments['__TEXT']
         return text_seg.vmaddr
 
     def get_bytes(self, offset, size):
+        # type: (int, int) -> str
+        """
+        Retrieve bytes from Mach-O slice, taking into account that the slice could be at an offset within a FAT
+        Args:
+            offset: index from beginning of slice to retrieve data from
+            size: maximum number of bytes to read
+
+        Returns:
+            string containing byte content of mach-o slice at an offset from the start of the slice
+        """
         # account for the fact that this Macho slice is not necessarily the start of the file!
         # add slide to our macho slice to file seek
         self._file.seek(offset + self.offset_within_fat)
         return self._file.read(size)
 
     def should_swap_bytes(self):
+        # type: (None) -> bool
+        """
+        Check whether self.magic refers to a big-endian Mach-O binary
+        Returns:
+            True if self.magic indicates a big endian Mach-O, False otherwise
+        """
         # TODO(pt): figure out whether we need to swap to little or big endian,
         # based on system endianness and binary endianness
         # everything we touch currently is little endian, so let's not worry about it for now
--- strongarm/macho_parse.py
+++ strongarm/macho_parse.py
@@ -4,6 +4,7 @@ from macho_binary import MachoBinary
 
 class MachoParser(object):
     def __init__(self, filename):
+        # type: (str) -> MachoParser
         self.is_swapped = False
         self.magic = 0
         self._file = None
@@ -15,6 +16,14 @@ class MachoParser(object):
         self.parse(filename)
 
     def parse(self, filename):
+        # type: (str) -> None
+        """
+        Parse a Mach-O or FAT archive
+        This method will throw an exception if an binary is passed which is malformed or not a
+        valid Mach-O or FAT archive
+        Args:
+            filename: path to binary to interpret
+        """
         self._file = open(filename, 'rb')
 
         self.magic, self.is_fat = self.check_magic()
@@ -31,10 +40,18 @@ class MachoParser(object):
             self.header = self.parse_thin_header(0)
 
     def parse_thin_header(self, fileoff):
+        # type: (int) -> None
+        """
+        Parse a known Mach-O header at a given file offset, and add it to self.slices
+        This method will throw an Exception if the data at fileoff is not a valid Mach-O header
+        Args:
+            fileoff: byte index into file to interpret Mach-O header at
+        """
         # sanity check
         if not self._check_is_macho_header(fileoff):
             raise RuntimeError('Parsing error: MachO archive at {} was not a valid Macho archive!'.format(hex(fileoff)))
 
+        # MachoBinary constructor will throw an exception if the header can't be parsed
         try:
             attempt = MachoBinary(self._file, fileoff)
             # if the MachoBinary does not have a header, there was a problem parsing it
@@ -43,21 +60,39 @@ class MachoParser(object):
         except RuntimeError as e:
             pass
 
-    def _is_little_endian(self, magic):
+    @staticmethod
+    def _is_little_endian(magic):
+        # type: (int) -> bool
+        """
+        Check whether a given magic represents a little endian MachO or FAT archive
+        Args:
+            magic: magic value to check type of
+
+        Returns:
+            True if the magic corresponds to a little endian Mach-O or FAT archive, False otherwise
+        """
         return magic in [MachArch.MH_MAGIC,
                          MachArch.MH_MAGIC_64,
                          MachArch.FAT_MAGIC,
                          ]
 
     def parse_fat_header(self):
+        """
+        Parse the FAT header implicitly found at the start of the file
+        This method will also parse all Mach-O's that the FAT describes
+        """
         # sanity check
         if self._check_is_macho_header(0):
             raise RuntimeError('Parsing error: Expected FAT header but found incorrect magic!')
 
+
+        # start reading from the start of the file
         read_off = 0
         self.header = MachoFatHeader.from_buffer(bytearray(self.get_bytes(0, sizeof(MachoFatHeader))))
+        # first fat_arch structure is directly after FAT header
         read_off += sizeof(MachoFatHeader)
 
+        # remember to swap fields if file contains non-native byte order
         if self.is_swapped:
             self.header.nfat_arch = swap32(self.header.nfat_arch)
 
@@ -69,6 +104,7 @@ class MachoParser(object):
             # do we need to byte swap?
             # TODO(pt): come up with more elegant mechanism for swapping byte order in every word of Structure
             if self.is_swapped:
+                # non-native byte order, swap every field in fat_arch
                 fat_arch.cputype = swap32(int(fat_arch.cputype))
                 fat_arch.cpusubtype = swap32(int(fat_arch.cpusubtype))
                 fat_arch.offset = swap32(int(fat_arch.offset))
@@ -76,11 +112,21 @@ class MachoParser(object):
                 fat_arch.align = swap32(int(fat_arch.align))
 
             self.parse_thin_header(fat_arch.offset)
+            # move to next fat_arch structure in file
             read_off += sizeof(MachoFatArch)
 
         return self.header
 
     def _check_is_macho_header(self, offset):
+        # type: (int) -> bool
+        """
+        Check if the bytes located at a file offset represents a valid Mach-O header, based on the magic
+        Args:
+            offset: File offset to read magic from
+        Returns:
+            True if the byte content of the file at 'offset' contain the magic number for Mach-O slices,
+            False if the content is something else
+        """
         self._file.seek(offset)
         magic = c_uint32.from_buffer(bytearray(self._file.read(sizeof(c_uint32)))).value
         macho_magics = [MachArch.MH_MAGIC,
@@ -91,6 +137,14 @@ class MachoParser(object):
         return magic in macho_magics
 
     def _is_supported(self, magic):
+        # type: (int) -> bool
+        """
+        Check whether a magic number represents a file format which this class is capable of parsing
+        Args:
+            magic: Magic value denoting file type to check against
+        Returns:
+            True if the magic number represents a supported file format, False otherwise
+        """
         supported = [
             MachArch.FAT_MAGIC,
             MachArch.FAT_CIGAM,
@@ -100,6 +154,14 @@ class MachoParser(object):
         return magic in supported
 
     def check_magic(self, offset=0):
+        # type: (int) -> (int, bool)
+        """
+        Parse file magic and return the magic, and whether the format represents a FAT
+        Args:
+            offset: File offset to read the header from
+        Returns:
+            A tuple containing the magic that was read, and a bool indicating whether the header is a FAT
+        """
         self._file.seek(offset)
         magic = c_uint32.from_buffer(bytearray(self._file.read(sizeof(c_uint32)))).value
         is_fat = False
@@ -107,27 +169,26 @@ class MachoParser(object):
         if magic == MachArch.FAT_MAGIC:
             print('FAT archive @ {} detected (FAT in little endian)'.format(hex(offset)))
             is_fat = True
-            return magic, is_fat
         elif magic == MachArch.FAT_CIGAM:
             print('FAT archive @ {} detected (FAT in big endian)'.format(hex(offset)))
             is_fat = True
-            return magic, is_fat
-
         # what kind of Mach O?
-        if magic == MachArch.MH_MAGIC or magic == MachArch.MH_CIGAM:
+        elif magic == MachArch.MH_MAGIC or magic == MachArch.MH_CIGAM:
             print('32-bit Mach-O binaries not yet supported.')
-            return magic, is_fat
         elif magic == MachArch.MH_MAGIC_64:
             print('64-bit Mach-O magic ok @ {}  (little endian)'.format(hex(offset)))
-            return magic, is_fat
         elif magic == MachArch.MH_CIGAM_64:
             print('64-bit Mach-O magic ok @ {} (big endian)'.format(hex(offset)))
-            return magic, is_fat
         # unknown magic!
         print('Unrecognized file magic {}'.format(hex(magic)))
-        return False
+        return magic, is_fat
 
     def should_swap_bytes(self):
+        """
+        Check if we need to swap due to a difference in endianness between host and binary
+        Returns:
+            True if the host and binary differ in endianness, False otherwise
+        """
         # TODO(pt): figure out whether we need to swap to little or big endian,
         # based on system endianness and binary endianness
         # everything we touch currently is little endian, so let's not worry about it for now
@@ -138,8 +199,20 @@ class MachoParser(object):
         return self.magic in big_endian
 
     def get_bytes(self, offset, size):
+        # type: (int, int) -> str
+        """
+        Retrieve a string containing up to 'size' bytes beginning from file offset 'offset'
+        Args:
+            offset: Offset within file to begin reading from
+            size: Maximum number of bytes to read
+
+        Returns:
+            String representing byte contents of file at provided address
+        """
         self._file.seek(offset)
         return self._file.read(size)
 
     def __del__(self):
+        # don't waste this file descriptor!
+        # close open file once the object is destroyed
         self._file.close()

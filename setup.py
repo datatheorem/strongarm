e2d2208e792be8adf9c26169de16d121aa2254a5
Phillip Tennen <pt@datatheorem.com>
2017-10-30 13:07:52 -0400

    setup.py runs install_dependencies.sh

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+from subprocess import call
 
 from strongarm import __version__
 
+
+class CapstoneInstall(install):
+    def run(self):
+        call(['/bin/sh', './install_dependencies.sh'])
+        self.do_egg_install()
 
 setup(
     name='strongarm',
@@ -10,6 +17,9 @@ setup(
     author='Data Theorem',
     url='https://bitbucket.org/datatheorem/strongarm',
     packages=find_packages(exclude=['tests']),
+    cmdclass={
+        'install': CapstoneInstall,
+    },
     install_requires=[
         'typing',
         'capstone',

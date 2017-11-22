e392e8eb471ce7b13abdded4f367ee30cf63dbca
Phillip Tennen <pt@datatheorem.com>
2017-11-21 19:16:24 -0500

    clean up PoC STA-183 check a bit

--- sta-183.py
+++ sta-183.py
@@ -1,12 +1,7 @@
 from strongarm.macho.macho_analyzer import MachoAnalyzer
 from strongarm.macho.macho_parse import MachoParser
-from strongarm.macho.macho_binary import MachoBinary
-from strongarm.objc.objc_analyzer import ObjcFunctionAnalyzer
 from strongarm.objc.objc_query import ObjcPredicateBranchQuery
 from strongarm.debug_util import DebugUtil
-
-from strongarm.macho.macho_definitions import CFStringStruct
-from ctypes import c_uint64, sizeof
 
 import njas
 
@@ -22,19 +17,18 @@ matches = analyzer.perform_query([nsclass_from_string_check, objc_get_class_chec
 
 sym_resolver = njas.resolve_sym.SymbolResolver()
 
+
+def print_dlopen_finding(search_result, function_name, function_arg):
+    print('-[{} {}]'.format(search_result.objc_class.name, search_result.objc_selector.name))
+    print('\t{}({})'.format(function_name, function_arg))
+
+
+def print_symbol_finding(search_result, function_name, function_arg):
+    print('-[{} {}]'.format(search_result.objc_class.name, search_result.objc_selector.name))
+    print('\t{}({})'.format(function_name, function_arg))
+    print('\t\t{}'.format(source_binary_path))
+
 for result in matches:
-    found_func = ''
-    if result.predicate_list == nsclass_from_string_check:
-        found_func = 'NSClassFromString'
-    elif result.predicate_list == objc_get_class_check:
-        found_func = 'objc_getClass'
-    elif result.predicate_list == dlopen_check:
-        found_func = 'dlopen'
-    print('found result at {}'.format(hex(int(result.function_analyzer.start_address))))
-
-    import sys
-    #sys.exit(0)
-
     func = result.function_analyzer
     call_idx = func.instructions.index(result.instruction)
     function_arg, depends_on_argument = func.determine_register_contents('x0', start_index=call_idx)
@@ -48,10 +42,22 @@ for result in matches:
             pass
     else:
         function_arg = binary.read_embedded_string(int(function_arg))
-    print('-[{} {}]'.format(result.objc_class.name, result.objc_selector.name))
-    print('\t{}({})'.format(found_func, function_arg))
 
+    found_func = ''
+    if result.predicate_list == dlopen_check:
+        found_func = 'dlopen'
+        if 'PrivateFrameworks' in function_arg:
+            print_dlopen_finding(result, found_func, function_arg)
+        continue
+
+    elif result.predicate_list == nsclass_from_string_check:
+        found_func = 'NSClassFromString'
+    elif result.predicate_list == objc_get_class_check:
+        found_func = 'objc_getClass'
+
+    # check if the symbol passed to the function was private
     # typically, symbols in symbol tables are prepended with _
-    class_symbol = '_{}'.format(function_arg)
-    source_binary_path = sym_resolver.binary_for_symbol(class_symbol)
-    print('\t\t{}'.format(source_binary_path))
+    full_symbol_name = '_{}'.format(function_arg)
+    source_binary_path = sym_resolver.binary_for_symbol(full_symbol_name)
+    if source_binary_path:
+        print_symbol_finding(result, found_func, function_arg)

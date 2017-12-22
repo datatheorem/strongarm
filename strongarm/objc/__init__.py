af3164991486c30393c858cd3c9a2110707029f1
Phillip Tennen <pt@datatheorem.com>
2017-12-22 13:16:24 -0500

    rename code search classes

--- strongarm/objc/__init__.py
+++ strongarm/objc/__init__.py
@@ -3,15 +3,25 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 from __future__ import print_function
 
-from .objc_query import ObjcPredicateQuery, \
-    ObjcPredicateBranchQuery, \
-    ObjcPredicateMnemonicQuery, \
-    ObjcPredicateOperandQuery, \
-    ObjcPredicateInstructionIndexQuery, \
-    ObjcPredicateRegisterContentsQuery
-from .objc_analyzer import ObjcFunctionAnalyzer, ObjcBlockAnalyzer
-from .objc_instruction import ObjcBranchInstruction, \
+from .objc_analyzer import \
+    ObjcFunctionAnalyzer, \
+    ObjcBlockAnalyzer
+
+from .objc_query import \
+    CodeSearch, \
+    CodeSearchTerm, \
+    CodeSearchTermCallDestination, \
+    CodeSearchTermInstructionIndex, \
+    CodeSearchTermInstructionMnemonic, \
+    CodeSearchTermInstructionOperand, \
+    CodeSearchTermRegisterContents, \
+    CodeSearchResult
+
+from .objc_instruction import \
+    ObjcBranchInstruction, \
     ObjcUnconditionalBranchInstruction, \
     ObjcConditionalBranchInstruction, \
     ObjcInstruction
-from .objc_basic_block import ObjcBasicBlock
+
+from .objc_basic_block import \
+    ObjcBasicBlock

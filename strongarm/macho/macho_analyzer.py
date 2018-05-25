4c4aa4d6561b3cb7c279b82e2b885926061b6eb9
Phillip Tennen <pt@datatheorem.com>
2018-05-25 11:36:43 +0200

    only calculate code search % completion if entry point list is big enough to be split into 10 divisions -_-
    Former-commit-id: 160270b541cfcdeb7a04548f0c4f96f45fd970f7

--- strongarm/macho/macho_analyzer.py
+++ strongarm/macho/macho_analyzer.py
@@ -417,9 +417,10 @@ class MachoAnalyzer(object):
 
             if len(entry_point_list):
                 checkpoint_index = int(len(entry_point_list) / 10)
-                if i % checkpoint_index == 0:
-                    percent_complete = int((i / checkpoint_index) * 10)
-                    logging.info(f'binary code search {percent_complete}% complete')
+                if checkpoint_index:
+                    if i % checkpoint_index == 0:
+                        percent_complete = int((i / checkpoint_index) * 10)
+                        logging.info(f'binary code search {percent_complete}% complete')
         return search_results
 
     def class_name_for_class_pointer(self, classref: int) -> Optional[str]:

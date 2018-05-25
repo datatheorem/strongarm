10e328736b0200d6107e2cc21951693b2f8c981c
Phillip Tennen <pt@datatheorem.com>
2018-05-25 11:20:07 +0200

    only calculate code search % completion if the entry point list isn't empty -_-
    Former-commit-id: feb16bf395a7d79e79a91b13e4a3e2ceb9f50dbb

--- strongarm/macho/macho_analyzer.py
+++ strongarm/macho/macho_analyzer.py
@@ -415,10 +415,11 @@ class MachoAnalyzer(object):
             except RuntimeError:
                 continue
 
-            checkpoint_index = int(len(entry_point_list) / 10)
-            if i % checkpoint_index == 0:
-                percent_complete = int((i / checkpoint_index) * 10)
-                logging.info(f'binary code search {percent_complete}% complete')
+            if len(entry_point_list):
+                checkpoint_index = int(len(entry_point_list) / 10)
+                if i % checkpoint_index == 0:
+                    percent_complete = int((i / checkpoint_index) * 10)
+                    logging.info(f'binary code search {percent_complete}% complete')
         return search_results
 
     def class_name_for_class_pointer(self, classref: int) -> Optional[str]:

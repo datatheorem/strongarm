8c73618d5012173ab7b8b4734fec58c336d0766d
Phillip Tennen <pt@datatheorem.com>
2018-06-06 18:05:59 +0200

    add capstone library to python extension config
    Former-commit-id: 2a594ab88af5f508389e8ace29b1dddcf76fe595

--- strongarm/objc/dataflow/setup.py
+++ strongarm/objc/dataflow/setup.py
@@ -1,4 +1,6 @@
 from distutils.core import setup, Extension
 
-dataflow_module = Extension('dataflow', sources=['dataflow.c'])
+dataflow_module = Extension('dataflow', 
+                             sources=['dataflow.cpp'],
+                             libraries = ['capstone'])
 setup(ext_modules=[dataflow_module])

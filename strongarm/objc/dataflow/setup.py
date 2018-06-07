6760efd8d870191cb5f95d634f6141e9364dd78b
Phillip Tennen <pt@datatheorem.com>
2018-06-07 14:45:54 +0200

    dataflow module is a sub-package instead of its own package
    Former-commit-id: 2e526e12df8b0f808eca832147ec267fd7b13a35

--- strongarm/objc/dataflow/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup, Extension
-
-dataflow_module = Extension('dataflow', 
-                             sources=['dataflow.cpp'],
-                             libraries = ['capstone'])
-setup(ext_modules=[dataflow_module])

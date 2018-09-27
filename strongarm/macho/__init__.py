f1e41163a3218ac75485487f7bcc9926af89c17d
Phillip Tennen <pt@datatheorem.com>
2018-09-27 15:45:56 +0200

    add tests for codesign parser

--- strongarm/macho/__init__.py
+++ strongarm/macho/__init__.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import unicode_literals
-from __future__ import print_function
-
 from .arch_independent_structs import *
 from .macho_parse import *
 from .macho_binary import *
--- strongarm/objc/__init__.py
+++ strongarm/objc/__init__.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import unicode_literals
-from __future__ import print_function
-
 from .register_contents import RegisterContents, RegisterContentsType
 
 from .objc_query import \

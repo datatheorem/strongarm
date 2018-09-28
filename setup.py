ccc3cb76f45b66847a15dd713243516e4257d984
Phillip Tennen <pt@datatheorem.com>
2018-09-28 15:49:25 +0200

    Revert "remove custom search path"
    This reverts commit 80b2f80b6de904f63cefe8277116fd77a59a61fe.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from subprocess import call
+import os
+from subprocess import call, check_output
 
 from setuptools import Extension, find_packages, setup
 from setuptools.command.build_ext import build_ext
@@ -12,9 +13,16 @@ class CapstoneBuild(build_ext):
         super(CapstoneBuild, self).run()
 
 
+# EA: when in a (my?) pipenv enviroment, clang does not inherit the c++ header search path. Manually find and provide
+# This is only needed on macOS
+xcode_cpp_search_path = ''
+if 'Darwin' in os.name:
+    xcode_path = check_output('xcode-select -p', shell=True)
+    xcode_cpp_search_path = f'{xcode_path}/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1'
+
 dataflow_module = Extension('strongarm.objc.dataflow',
                             sources=['strongarm/objc/dataflow.cpp'],
-                            include_dirs=['/usr/local/include/', '/usr/include/'],
+                            include_dirs=['/usr/local/include/', '/usr/include/', xcode_cpp_search_path],
                             libraries=['capstone'],
                             library_dirs=['/usr/local/lib'],
                             language='c++',

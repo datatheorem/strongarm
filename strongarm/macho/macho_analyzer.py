4ae98189c2a0cfe107f0056891ff272e8b5e42d3
Phillip Tennen <pt@datatheorem.com>
2019-09-09 16:45:29 +0200

    add option to disable CodeSearch progress bar

--- strongarm/macho/macho_analyzer.py
+++ strongarm/macho/macho_analyzer.py
@@ -271,7 +271,7 @@ class MachoAnalyzer:
         logging.info(f'{binary_name} enqueuing CodeSearch {code_search}. Will invoke {callback}')
         self._queued_code_searches[code_search] = callback
 
-    def search_all_code(self) -> None:
+    def search_all_code(self, display_progress: bool = True) -> None:
         """Iterate every function in the binary, and run each pending CodeSearch over them.
         The search space is all known Objective-C entry points within the binary.
 
@@ -297,7 +297,7 @@ class MachoAnalyzer:
         # Searching all code can be a time-consumptive operation. Provide UI feedback on the progress.
         # This displays a progress bar to stdout. The progress bar will be erased when the context manager exits.
         code_size = self.binary.slice_filesize / 1024 / 1024
-        with ConsoleProgressBar(prefix=f'CodeSearch {int(code_size)}mb') as progress_bar:
+        with ConsoleProgressBar(prefix=f'CodeSearch {int(code_size)}mb', enabled=display_progress) as progress_bar:
             for i, method_info in enumerate(entry_point_list):
                 try:
                     function_analyzer = ObjcFunctionAnalyzer.get_function_analyzer_for_method(self.binary, method_info)

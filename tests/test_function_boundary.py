1a11a8d7cc89022d9ebeac546398a8f6ed2e1236
Alexis Pierru <ap@sourcetheorem.com>
2019-12-20 14:20:14 +0100

    Remove determine_function_boundary

--- tests/test_function_boundary.py
+++ tests/test_function_boundary.py
@@ -3,7 +3,6 @@ from ctypes import create_string_buffer
 
 from strongarm.macho.macho_parse import MachoParser
 from strongarm.macho.macho_analyzer import MachoAnalyzer
-from strongarm.objc.dataflow import determine_function_boundary
 
 
 class TestFunctionBoundary:
@@ -13,38 +12,6 @@ class TestFunctionBoundary:
         parser = MachoParser(TestFunctionBoundary.FAT_PATH)
         self.binary = parser.slices[0]
         self.analyzer = MachoAnalyzer.get_analyzer(self.binary)
-
-    def test_function_boundary_ret(self):
-        # found in Hopper
-        # address of -[DTHAppDelegate application:didFinishLaunchingWithOptions:]
-        # this function ends with a ret instruction
-        start_address = 0x1000066dc
-        end_address = 0x1000066e0
-
-        # defined in MachoAnalyzer
-        max_function_size = 0x2000
-        binary_data = self.binary.get_content_from_virtual_address(start_address, max_function_size)
-        bytecode = create_string_buffer(bytes(binary_data), max_function_size)
-        # not in cache. calculate function boundary, then cache it
-        guessed_end_address = determine_function_boundary(bytecode, start_address)
-
-        assert guessed_end_address == end_address
-
-    def test_function_boundary_bl(self):
-        # found in Hopper
-        # address of -[DTHAppDelegate setWindow:]
-        # this function ends with a b/bl instruction
-        start_address = 0x100006708
-        end_address = 0x100006718
-
-        # defined in MachoAnalyzer
-        max_function_size = 0x2000
-        binary_data = self.binary.get_content_from_virtual_address(start_address, max_function_size)
-        bytecode = create_string_buffer(bytes(binary_data), max_function_size)
-        # not in cache. calculate function boundary, then cache it
-        guessed_end_address = determine_function_boundary(bytecode, start_address)
-
-        assert guessed_end_address == end_address
 
     def test_find_method_code(self):
         sel = 'application:didFinishLaunchingWithOptions:'

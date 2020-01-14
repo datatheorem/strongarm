f5110ff04f38b27dbfdfb6cd9bd4b4c518ad64bd
Alexis Pierru <ap@sourcetheorem.com>
2020-01-14 15:55:34 +0100

    Update tests

--- tests/test_basic_blocks.py
+++ tests/test_basic_blocks.py
@@ -23,12 +23,12 @@ class TestBasicBlocks:
         # Then the basic-block-boundaries are correctly identified
         # (This isn't strictly correct since this source function contains a switch-table, but it's good enough for now)
         correct_basic_blocks = [
-            (0x10000675c, 0x100006790), (0x100006794, 0x1000067b0), (0x1000067b4, 0x1000067bc),
+            (0x10000675c, 0x100006770), (0x100006774, 0x100006774), (0x100006778, 0x100006790),
+            (0x100006794, 0x1000067a4), (0x1000067a8, 0x1000067b0), (0x1000067b4, 0x1000067bc),
             (0x1000067c0, 0x1000067c8), (0x1000067cc, 0x1000067d4), (0x1000067d8, 0x1000067dc),
-            (0x1000067e0, 0x1000067f0),
+            (0x1000067e0, 0x1000067e0), (0x1000067e4, 0x1000067f0),
         ]
-        correct_basic_blocks = [(VirtualMemoryPointer(s), VirtualMemoryPointer(e)) for s, e in correct_basic_blocks]
-        assert basic_blocks == correct_basic_blocks
+        assert basic_blocks == [(VirtualMemoryPointer(a), VirtualMemoryPointer(b)) for a, b in correct_basic_blocks]
 
     def test_find_basic_blocks_2(self):
         # Given I provide a method implementation with a backwards local jump
@@ -38,8 +38,12 @@ class TestBasicBlocks:
         basic_blocks = [(x.start_address, x.end_address) for x in function_analyzer.basic_blocks]
 
         # Then the basic-block-boundaries are correctly identified
-        correct_basic_blocks = [(0x100006804, 0x10000681c), (0x100006820, 0x100006834), (0x100006838, 0x100006844)]
-        assert basic_blocks == correct_basic_blocks
+        correct_basic_blocks = [
+            (0x100006804, 0x10000681c), (0x100006820, 0x100006828), (0x10000682c, 0x100006834),
+            (0x100006838, 0x100006844),
+        ]
+
+        assert basic_blocks == [(VirtualMemoryPointer(a), VirtualMemoryPointer(b)) for a, b in correct_basic_blocks]
 
     def test_find_basic_blocks3(self):
         # Given I provide a method implementation with a backwards local jump
@@ -49,8 +53,11 @@ class TestBasicBlocks:
         basic_blocks = [(x.start_address, x.end_address) for x in function_analyzer.basic_blocks]
 
         # Then the basic-block-boundaries are correctly identified
-        correct_basic_blocks = [(0x100006848, 0x100006860), (0x100006864, 0x100006878), (0x10000687c, 0x100006894)]
-        assert basic_blocks == correct_basic_blocks
+        correct_basic_blocks = [
+            (0x100006848, 0x100006860), (0x100006864, 0x10000686c), (0x100006870, 0x100006878),
+            (0x10000687c, 0x100006884), (0x100006888, 0x100006894),
+        ]
+        assert basic_blocks == [(VirtualMemoryPointer(a), VirtualMemoryPointer(b)) for a, b in correct_basic_blocks]
 
     def test_find_basic_blocks4(self):
         # Given I provide a function with no internal branching
@@ -60,8 +67,12 @@ class TestBasicBlocks:
         basic_blocks = [(x.start_address, x.end_address) for x in function_analyzer.basic_blocks]
 
         # Then I see one big basic block
-        correct_basic_blocks = [(0x100006898, 0x100006914)]
-        assert basic_blocks == correct_basic_blocks
+        correct_basic_blocks = [
+            (0x100006898, 0x1000068b0), (0x1000068b4, 0x1000068c8), (0x1000068cc, 0x1000068cc),
+            (0x1000068d0, 0x1000068d4), (0x1000068d8, 0x1000068ec), (0x1000068f0, 0x1000068f8),
+            (0x1000068fc, 0x100006900), (0x100006904, 0x100006914),
+        ]
+        assert basic_blocks == [(VirtualMemoryPointer(a), VirtualMemoryPointer(b)) for a, b in correct_basic_blocks]
 
     def test_find_basic_blocks5(self):
         # Given I provide a method implementation with forwards local jumps
@@ -72,10 +83,12 @@ class TestBasicBlocks:
 
         # Then the basic-block-boundaries are correctly identified
         correct_basic_blocks = [
-            (0x1000066e4, 0x10000671c), (0x100006720, 0x100006720), (0x100006724, 0x10000672c),
-            (0x100006730, 0x100006738), (0x10000673c, 0x100006740), (0x100006744, 0x100006758)
+            (0x1000066e4, 0x1000066f4), (0x1000066f8, 0x1000066f8), (0x1000066fc, 0x10000671c),
+            (0x100006720, 0x100006720), (0x100006724, 0x10000672c), (0x100006730, 0x100006738),
+            (0x10000673c, 0x100006740), (0x100006744, 0x100006744), (0x100006748, 0x100006750),
+            (0x100006754, 0x100006758),
         ]
-        assert basic_blocks == correct_basic_blocks
+        assert basic_blocks == [(VirtualMemoryPointer(a), VirtualMemoryPointer(b)) for a, b in correct_basic_blocks]
 
     def test_find_control_flow6(self):
         # Given I provide a method implementation with both forwards and backwards local jumps
@@ -86,8 +99,9 @@ class TestBasicBlocks:
 
         # Then the basic-block-boundaries are correctly identified
         correct_basic_blocks = [
-            (0x100006648, 0x10000665c), (0x100006660, 0x100006680), (0x100006684, 0x10000668c),
-            (0x100006690, 0x1000066a4), (0x1000066a8, 0x1000066ac), (0x1000066b0, 0x1000066c0),
-            (0x1000066c4, 0x1000066cc), (0x1000066d0, 0x1000066e0),
+            (0x100006648, 0x100006658), (0x10000665c, 0x10000665c), (0x100006660, 0x100006660),
+            (0x100006664, 0x100006680), (0x100006684, 0x10000668c), (0x100006690, 0x100006698),
+            (0x10000669c, 0x1000066a4), (0x1000066a8, 0x1000066ac), (0x1000066b0, 0x1000066b8),
+            (0x1000066bc, 0x1000066c0), (0x1000066c4, 0x1000066cc), (0x1000066d0, 0x1000066e0),
         ]
-        assert basic_blocks == correct_basic_blocks
+        assert basic_blocks == [(VirtualMemoryPointer(a), VirtualMemoryPointer(b)) for a, b in correct_basic_blocks]

485c2759c7797536d2f99c0f8de9430b85c9d24f
Alexis Pierru <ap@sourcetheorem.com>
2020-01-24 17:34:40 +0100

    Update test cases for basic blocks

--- tests/test_basic_blocks.py
+++ tests/test_basic_blocks.py
@@ -22,10 +22,10 @@ class TestBasicBlocks:
 
         # Then the basic-block-boundaries are correctly identified
         correct_basic_blocks = [
-            (0x10000675c, 0x100006774), (0x100006774, 0x100006778), (0x100006778, 0x100006794),
-            (0x100006794, 0x1000067a8), (0x1000067a8, 0x1000067b4), (0x1000067b4, 0x1000067c0),
-            (0x1000067c0, 0x1000067cc), (0x1000067cc, 0x1000067d8), (0x1000067d8, 0x1000067e0),
-            (0x1000067e0, 0x1000067e4), (0x1000067e4, 0x1000067f4),
+            (0x10000675c, 0x100006770), (0x100006774, 0x100006774), (0x100006778, 0x100006790),
+            (0x100006794, 0x1000067a4), (0x1000067a8, 0x1000067b0), (0x1000067b4, 0x1000067bc),
+            (0x1000067c0, 0x1000067c8), (0x1000067cc, 0x1000067d4), (0x1000067d8, 0x1000067dc),
+            (0x1000067e0, 0x1000067e0), (0x1000067e4, 0x1000067f0),
         ]
         assert basic_blocks == [(VirtualMemoryPointer(a), VirtualMemoryPointer(b)) for a, b in correct_basic_blocks]
 
@@ -38,8 +38,8 @@ class TestBasicBlocks:
 
         # Then the basic-block-boundaries are correctly identified
         correct_basic_blocks = [
-            (0x100006804, 0x100006820), (0x100006820, 0x10000682c), (0x10000682c, 0x100006838),
-            (0x100006838, 0x100006848),
+            (0x100006804, 0x10000681c), (0x100006820, 0x100006828), (0x10000682c, 0x100006834),
+            (0x100006838, 0x100006844),
         ]
 
         assert basic_blocks == [(VirtualMemoryPointer(a), VirtualMemoryPointer(b)) for a, b in correct_basic_blocks]
@@ -53,8 +53,8 @@ class TestBasicBlocks:
 
         # Then the basic-block-boundaries are correctly identified
         correct_basic_blocks = [
-            (0x100006848, 0x100006864), (0x100006864, 0x100006870), (0x100006870, 0x10000687c),
-            (0x10000687c, 0x100006888), (0x100006888, 0x100006898),
+            (0x100006848, 0x100006860), (0x100006864, 0x10000686c), (0x100006870, 0x100006878),
+            (0x10000687c, 0x100006884), (0x100006888, 0x100006894),
         ]
         assert basic_blocks == [(VirtualMemoryPointer(a), VirtualMemoryPointer(b)) for a, b in correct_basic_blocks]
 
@@ -67,9 +67,9 @@ class TestBasicBlocks:
 
         # Then I see one big basic block
         correct_basic_blocks = [
-            (0x100006898, 0x1000068b4), (0x1000068b4, 0x1000068cc), (0x1000068cc, 0x1000068d0),
-            (0x1000068d0, 0x1000068d8), (0x1000068d8, 0x1000068f0), (0x1000068f0, 0x1000068fc),
-            (0x1000068fc, 0x100006904), (0x100006904, 0x100006918),
+            (0x100006898, 0x1000068b0), (0x1000068b4, 0x1000068c8), (0x1000068cc, 0x1000068cc),
+            (0x1000068d0, 0x1000068d4), (0x1000068d8, 0x1000068ec), (0x1000068f0, 0x1000068f8),
+            (0x1000068fc, 0x100006900), (0x100006904, 0x100006914),
         ]
         assert basic_blocks == [(VirtualMemoryPointer(a), VirtualMemoryPointer(b)) for a, b in correct_basic_blocks]
 
@@ -82,10 +82,10 @@ class TestBasicBlocks:
 
         # Then the basic-block-boundaries are correctly identified
         correct_basic_blocks = [
-            (0x1000066e4, 0x1000066f8), (0x1000066f8, 0x1000066fc), (0x1000066fc, 0x100006720),
-            (0x100006720, 0x100006724), (0x100006724, 0x100006730), (0x100006730, 0x10000673c),
-            (0x10000673c, 0x100006744), (0x100006744, 0x100006748), (0x100006748, 0x100006754),
-            (0x100006754, 0x10000675c),
+            (0x1000066e4, 0x1000066f4), (0x1000066f8, 0x1000066f8), (0x1000066fc, 0x10000671c),
+            (0x100006720, 0x100006720), (0x100006724, 0x10000672c), (0x100006730, 0x100006738),
+            (0x10000673c, 0x100006740), (0x100006744, 0x100006744), (0x100006748, 0x100006750),
+            (0x100006754, 0x100006758),
         ]
         assert basic_blocks == [(VirtualMemoryPointer(a), VirtualMemoryPointer(b)) for a, b in correct_basic_blocks]
 
@@ -98,9 +98,9 @@ class TestBasicBlocks:
 
         # Then the basic-block-boundaries are correctly identified
         correct_basic_blocks = [
-            (0x100006648, 0x10000665c), (0x10000665c, 0x100006660), (0x100006660, 0x100006664),
-            (0x100006664, 0x100006684), (0x100006684, 0x100006690), (0x100006690, 0x10000669c),
-            (0x10000669c, 0x1000066a8), (0x1000066a8, 0x1000066b0), (0x1000066b0, 0x1000066bc),
-            (0x1000066bc, 0x1000066c4), (0x1000066c4, 0x1000066d0), (0x1000066d0, 0x1000066e4),
+            (0x100006648, 0x100006658), (0x10000665c, 0x10000665c), (0x100006660, 0x100006660),
+            (0x100006664, 0x100006680), (0x100006684, 0x10000668c), (0x100006690, 0x100006698),
+            (0x10000669c, 0x1000066a4), (0x1000066a8, 0x1000066ac), (0x1000066b0, 0x1000066b8),
+            (0x1000066bc, 0x1000066c0), (0x1000066c4, 0x1000066cc), (0x1000066d0, 0x1000066e0),
         ]
         assert basic_blocks == [(VirtualMemoryPointer(a), VirtualMemoryPointer(b)) for a, b in correct_basic_blocks]

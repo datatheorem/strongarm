98299aa9faae338a0c2a957233e83ffdbc33d246
Phillip Tennen <pt@datatheorem.com>
2020-02-03 17:16:10 +0100

    [SCAN-1607] refactor linting task logic
    Alexis & Fede’s PR feedback

--- tasks.py
+++ tasks.py
@@ -42,7 +42,29 @@ def check_standards(ctx):
     # type: (Context) -> None
     """Check formatting of the code
     """
-    _run_linters(ctx, check=True)
+    # Gather all the modules / files in the tree
+    files_to_process = " ".join(_get_python_modules())
+
+    # Find virtual environment for isort
+    venv = ctx.run("pipenv --venv", hide=True).stdout.strip()
+
+    autoflake_version = ctx.run("autoflake --version", hide=True).stdout.strip().split()[1]
+    isort_version = ctx.run("isort --version | grep VERSION", hide=True).stdout.strip().split()[1]
+    black_version = ctx.run("black --version", hide=True).stdout.strip().split()[2]
+    flake8_version = ctx.run("flake8 --version", hide=True).stdout.strip().split()[0]
+
+    # Check that validations pass without modifying the code
+    print(f"Checking imports optimization (autoflake v{autoflake_version})")
+    ctx.run(f"autoflake --recursive {files_to_process}")  # Default behaviour is to print diff
+
+    print(f"Checking imports sorting (isort v{isort_version})")
+    ctx.run(f"isort --check --diff --virtual-env {venv} --recursive {files_to_process}")
+
+    print(f"Checking black format (black v{black_version})")
+    ctx.run(f"black --check --diff {files_to_process}")
+
+    print(f"Checking format (flake8 v{flake8_version})")
+    ctx.run(f"flake8 {files_to_process}")
 
 
 @task
@@ -50,40 +72,18 @@ def standards(ctx):
     # type: (Context) -> None
     """Run auto-formatting tools
     """
-    _run_linters(ctx, check=False)
-
-
-def _run_linters(ctx: Context, check: bool) -> None:
-    # Get the files to process
+    # Gather all the modules / files in the tree
     files_to_process = " ".join(_get_python_modules())
 
     # Find virtual environment for isort
     venv = ctx.run("pipenv --venv", hide=True).stdout.strip()
-    autoflake_version = ctx.run("autoflake --version", hide=True).stdout.strip().split()[1]
-    isort_version = ctx.run("isort --version | grep VERSION", hide=True).stdout.strip().split()[1]
-    black_version = ctx.run("black --version", hide=True).stdout.strip().split()[2]
-    flake8_version = ctx.run("flake8 --version", hide=True).stdout.strip().split()[0]
 
-    if check:
-        # Check that validations pass without modifying the code
-        print(f"Checking imports optimization (autoflake v{autoflake_version})")
-        ctx.run(f"autoflake --recursive {files_to_process}")  # Default behaviour is to print diff
+    # Run auto-formatting tools
+    print("Optimizing imports")
+    ctx.run(f"autoflake --in-place --recursive {files_to_process}")
 
-        print(f"Checking imports sorting (isort v{isort_version})")
-        ctx.run(f"isort --check --diff --virtual-env {venv} --recursive {files_to_process}")
+    print("Sorting imports")
+    ctx.run(f"isort --virtual-env {venv} --apply --recursive {files_to_process}")
 
-        print(f"Checking black format (black v{black_version})")
-        ctx.run(f"black --check --diff {files_to_process}")
-
-        print(f"Checking format (flake8 v{flake8_version})")
-        ctx.run(f"flake8 {files_to_process}")
-    else:
-        # Run auto-formatting tools
-        print("Optimizing imports")
-        ctx.run(f"autoflake --in-place --recursive {files_to_process}")
-
-        print("Sorting imports")
-        ctx.run(f"isort --virtual-env {venv} --apply --recursive {files_to_process}")
-
-        print("Blackifying code")
-        ctx.run(f"black {files_to_process}")
+    print("Blackifying code")
+    ctx.run(f"black {files_to_process}")

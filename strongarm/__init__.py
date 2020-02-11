1e046a9c1e27d8b03f432197586d8f566d88cbf2
Federico Sandri <fs@sourcetheorem.com>
2020-02-11 16:44:02 +0100

    Minor changes to project files

--- strongarm/__init__.py
+++ strongarm/__init__.py
@@ -1,5 +1,9 @@
+"""Mach-O/ARM64 analyzer"""
+
 from .debug_util import DebugUtil
 
 __version__ = "8.0.1"
+__author__ = "Data Theorem"
+__url__ = "https://bitbucket.org/datatheorem/strongarm"
 
 __all__ = ["DebugUtil"]

dfb6251342189ea838fdcf4a1b05ce370c6fb671
Federico Sandri <fs@sourcetheorem.com>
2020-02-11 16:54:18 +0100

    Unbreak flake8

--- strongarm/macho/macho_analyzer.py
+++ strongarm/macho/macho_analyzer.py
@@ -29,7 +29,7 @@ from strongarm.macho.objc_runtime_data_parser import (
 from strongarm.macho.progress_bar import ConsoleProgressBar
 
 if TYPE_CHECKING:
-    from strongarm.objc import ObjcFunctionAnalyzer, ObjcMethodInfo, CodeSearch, CodeSearchResult
+    from strongarm.objc import ObjcFunctionAnalyzer, ObjcMethodInfo, CodeSearch, CodeSearchResult  # noqa: F401
 
 
 _T = TypeVar("_T")

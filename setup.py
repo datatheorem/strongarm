e2c8755636052c314c1c8f1a814326216c0ac83d
Phillip Tennen <pt@datatheorem.com>
2020-04-09 16:00:53 +0200

    [SCAN-1658] fix typo in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=find_packages(exclude=["tests"]),
     install_requires=[
         "capstone",
-        "strongarm_dataflow @ git+ssh://git@bitbucket.org/datatheorem/strongarm_dataflow.git@1.0.0",
+        "strongarm_dataflow @ git+ssh://git@bitbucket.org/datatheorem/strongarm-dataflow.git@1.0.0",
     ],
     package_data={"strongarm": ["py.typed"]},
     data_files=[("", ["LICENSE.txt"])],

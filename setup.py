0ab644be18f3db1cac120cdad09db01d6cd076a7
Phillip Tennen <pt@datatheorem.com>
2020-10-07 15:49:16 +0200

    [SCAN-2216] bump strongarm-dataflow to 1.1.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=[
         "capstone",
         "more_itertools",
-        "strongarm_dataflow @ git+ssh://git@bitbucket.org/datatheorem/strongarm-dataflow.git@SCAN-2216-basic-block-cpp",
+        "strongarm_dataflow @ git+ssh://git@bitbucket.org/datatheorem/strongarm-dataflow.git@1.1.0",
     ],
     package_data={"strongarm": ["py.typed"]},
     data_files=[("", ["LICENSE.txt"])],

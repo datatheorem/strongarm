43c9b9e39523bc63e6bff9c8a5c8f756a30dc682
Phillip Tennen <pt@datatheorem.com>
2020-10-28 13:04:12 -0400

    [SCAN-2305] use feature strongarm branch
    [SCAN-2305] pipfile.lock

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=[
         "capstone",
         "more_itertools",
-        "strongarm_dataflow @ git+ssh://git@bitbucket.org/datatheorem/strongarm-dataflow.git@2.0.1",
+        "strongarm_dataflow @ git+ssh://git@bitbucket.org/datatheorem/strongarm-dataflow.git@SCAN-2305-all-cpp-xrefs",
     ],
     package_data={"strongarm": ["py.typed"]},
     data_files=[("", ["LICENSE.txt"])],

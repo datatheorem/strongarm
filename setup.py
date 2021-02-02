3cbb96d1b8a169c47baf1eed43ee4be36fc31831
Phillip Tennen <pt@datatheorem.io>
2021-02-02 13:08:11 +0100

    [SCAN-783] Blacken

--- setup.py
+++ setup.py
@@ -9,11 +9,7 @@ setup(
     author="Data Theorem",
     url="https://github.com/datatheorem/strongarm",
     packages=find_packages(exclude=["tests"]),
-    install_requires=[
-        "capstone",
-        "more_itertools",
-        "strongarm_dataflow==2.1.2",
-    ],
+    install_requires=["capstone", "more_itertools", "strongarm_dataflow==2.1.2",],
     package_data={"strongarm": ["py.typed"]},
     data_files=[("", ["LICENSE.txt"])],
 )

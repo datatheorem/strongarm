d62101f6559a23a9cde6f780fbce39f83f7972ce
Phillip Tennen <pt@datatheorem.io>
2023-09-06 12:53:46 +0100

    [SCAN-4142] Revert adding in FAT offsets

--- strongarm/macho/macho_binary.py
+++ strongarm/macho/macho_binary.py
@@ -629,9 +629,6 @@ class MachoBinary:
         # https://reverseengineering.stackexchange.com/questions/8177/convert-mach-o-vm-address-to-file-offset
         binary_address = (virtual_address - section_for_address.address) + section_for_address.offset
 
-        # We might be a slice within a FAT, so add in our slice offset.
-        binary_address += self.file_offset
-
         return StaticFilePointer(binary_address)
 
     def get_content_from_virtual_address(self, virtual_address: VirtualMemoryPointer, size: int) -> bytearray:
